cbc1c7811eee6a8fb4fa31c7b18baaa39f356035
CodeArtByVivian <zvivian0108@gmail.com>
2025-01-08 23:37:16 -0500

    Update test_download.py

--- recommender_app/test_download.py
+++ recommender_app/test_download.py
@@ -1,19 +1,17 @@
 import gdown
 import os
 
-# Updated file ID
+# Google Drive file ID and output file
 file_id = "1AIRzNPxLJDmZ7ELSV88soSHTE-IM6QBQ"
 output_file = "X_item_features.npy"
 
+# Construct the URL
 url = f"https://drive.google.com/uc?id={file_id}"
 
+# Check if the file already exists
 if not os.path.exists(output_file):
     print(f"{output_file} not found. Downloading from Google Drive...")
-    try:
-        gdown.download(url, output_file, quiet=False)
-        print(f"Downloaded {output_file} successfully.")
-    except Exception as e:
-        print(f"Error: {e}")
+    gdown.download(url, output_file, quiet=False)
 else:
     print(f"{output_file} already exists.")
 
